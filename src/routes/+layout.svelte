5504c5f0e2daba31857f1b0f859dc38828ef0387
Isaac Hunter <ikehunter5@gmail.com>
2025-06-29 12:10:17 -0400

    fix logout issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,19 @@
 	import '../app.css';
 
 	let { data, children } = $props();
-	let { session, supabase, user } = $derived(data);
+	let { session, supabase } = $derived(data);
 
 	// Initialize with data on first render
 	let userState = setUserState({ session: data.session, supabase: data.supabase, user: data.user });
 
-	$effect(() => {
-		// After first render, these are the most updated values
-		userState.updateState({ session, supabase, user });
-	});
+	// $effect(() => {
+	// 	// After first render, these are the most updated values
+	// 	userState.updateState({ session, supabase, user });
+	// });
 
 	$effect(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			userState.updateState({ session: newSession, supabase, user: newSession?.user || null });
 			if (newSession?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
 			}
